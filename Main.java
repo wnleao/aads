11df03157dd799c763d218773767c9380835d575
Wilson Leão <wln1987@gmail.com>
2021-12-04 15:18:48 +0000

    feat: chapter01 exercise09

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import exercises.chapter01.*;
 
 class Main {
   public static void main(String[] args) {
-    Ex08ZeroRowColumn.main(args);
+    Ex09Rotation.main(args);
   }
 }
--- exercises/chapter01/Ex09Rotation.java
+++ exercises/chapter01/Ex09Rotation.java
@@ -1,15 +1,62 @@
 package exercises.chapter01;
 
+import java.util.Scanner;
+
 public class Ex09Rotation {
 
   private static boolean isSubstring(String s1, String s2) {
     return s1.contains(s2);
   }
 
+  private static boolean isRotationA(String s1, String s2) {
+    if (s1.length() != s2.length()) {
+      return false;
+    }
+    if (s1.equals(s2)) {
+      return true;
+    }
+    return isSubstring(s1+s1, s2);
+  }
+  private static boolean isRotationB(char[] s1, char[] s2) {
+    if (s1.length != s2.length) {
+      return false;
+    }
+    if (s1.length == 0) {
+      return true;
+    }
+    int i = 0;
+    int j = 0;
+    while (j+i < s2.length) {
+      if (s1[i] == s2[j+i]) {
+        i++;
+        continue;
+      }
+      i = 0;
+      j++;
+    }
+    if (s1.length - i != j) {
+      return false;
+    }
+    for (int k = 0; k < j; k++) {
+      if (s1[i+k] != s2[k]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
-    String s1 = "taterbottle";
-    String s2 = "etaterbottl";
-    
+    try (Scanner in = new Scanner(System.in)) {
+      while (true) {
+        System.out.print("s1: ");
+        String s1 = in.nextLine(); // "bottletater"
+        System.out.print("s2: ");
+        String s2 = in.nextLine(); // "etaterbottl"
+        System.out.println("s1, s2 = '" + s1 + "', '" + s2 + "'");
+        System.out.println("OutputA: " + isRotationA(s1, s2));
+        System.out.println("OutputB: " + isRotationB(s1.toCharArray(), s2.toCharArray()));
+      } 
+    } 
   }
 
 }
