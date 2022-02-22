c4fee5abe08258a53fb17d3dd83944e04dac2e28
Wilson Leão <wln1987@gmail.com>
2022-02-22 00:58:18 +0000

    feat: C04E08 - first common ancestor
    - do not use parent link

--- exercises/chapter04/C04E08.java
+++ exercises/chapter04/C04E08.java
@@ -4,6 +4,9 @@ import exercises.Exercise;
 import datastructures.tree.Node;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
   * 4.8 First Common Ancestor
@@ -14,7 +17,8 @@ public class C04E08 extends Exercise {
     super(args);
   } 
 
-  public <T> Node<T> commonAncestor(Node<T> a, Node<T> b) {
+  public <T> Node<T> commonAncestorWPL(Node<T> a, Node<T> b) {
+    if (a.parent == null || b.parent == null) return null;
     if (a == b) return a.parent;
     Set<Node<T>> parents = new HashSet<>();    
     for (Node<T> p = a.parent; p != null; p = p.parent) {
@@ -25,8 +29,39 @@ public class C04E08 extends Exercise {
     return ca;
   }
 
-  private void dumpCommonAncestor(Node<String> a, Node<String> b, String expected) {
-    System.out.println(String.format("The common ancestor of %s and %s is %s and should be %s", a, b, commonAncestor(a, b), expected));
+  public <T> Node<T> commonAncestorNPL(Node<T> r, Node<T> a, Node<T> b) {
+    if (a.parent == null || b.parent == null) return null;
+    if (a == b) return a.parent;
+
+    // search depth first to find nodes a AND b parents
+    List<Node<T>> parentsA = new ArrayList<>();
+    findNodeParents(parentsA, r, a);
+    List<Node<T>> parentsB = new ArrayList<>();
+    findNodeParents(parentsB, r, b);
+    
+    // compare FIFO lists each node at a time to find common ancestor
+    Node<T> ca = null;
+    for(int i = 0; checkParents(parentsA, parentsB, i); ca = parentsA.get(i++));
+
+    return ca;
+  }
+
+  private <T> boolean checkParents(List<Node<T>> pas, List<Node<T>> pbs, int i) {
+    return i < pas.size() && i < pbs.size() && pas.get(i) == pbs.get(i);
+  } 
+
+  private <T> boolean findNodeParents(List<Node<T>> ps, Node<T> c, Node<T> n) {
+    if (c == n) return true;
+    if (c == null || c.left == null && c.right == null) return false;
+    ps.add(c);
+    if (findNodeParents(ps, c.left, n) || findNodeParents(ps, c.right, n)) return true;
+    ps.remove(ps.size() - 1);
+    return false;
+  }
+
+  private void dumpCommonAncestor(Node<String> r, Node<String> a, Node<String> b, String expected) {
+    System.out.println(String.format("NPL: the common ancestor of %s and %s is %s and should be %s", a, b, commonAncestorNPL(r, a, b), expected));
+     System.out.println(String.format("WPL: The common ancestor of %s and %s is %s and should be %s", a, b, commonAncestorWPL(a, b), expected));
   }
 
   public void compute(String[] args) {
@@ -54,10 +89,13 @@ public class C04E08 extends Exercise {
     n1.right.right.setLeft(Node.of("N"));
     n1.right.right.setRight(Node.of("O"));
     
-    dumpCommonAncestor(n1.left.left.left, n1.right.left.right, "A");
-    dumpCommonAncestor(n1.left.left.left, n1.left.left.right, "D");
-    dumpCommonAncestor(n1.right.left.left, n1.right.left.right, "F");
-    dumpCommonAncestor(n1.left.left.left, n1.left.left, "B");
+    dumpCommonAncestor(n1, n1.left.left.left, n1.right.left.right, "A");
+    dumpCommonAncestor(n1, n1.left, n1.right, "A");
+    dumpCommonAncestor(n1, n1.left, n1.left, "A");
+    dumpCommonAncestor(n1, n1, n1.right.left.right, "null");
+    dumpCommonAncestor(n1, n1.left.left.left, n1.left.left.right, "D");
+    dumpCommonAncestor(n1, n1.right.left.left, n1.right.left.right, "F");
+    dumpCommonAncestor(n1, n1.left.left.left, n1.left.left, "B");
   }
 
 }
